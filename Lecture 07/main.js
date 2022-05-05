a144551f0ffe2eb98fe8ff95dd5fdf2aeceb4e55
재우 박 <mobuk1113@office.yuhan.ac.kr>
2022-05-05 13:00:32 +0900

    [JW] 10강 업로드

---
+++ Lecture 07/main.js
@@ -0,0 +1,16 @@
+var test = document.getElementById("test");
+
+function Init(){
+    test.innerHTML = "<p id='cc'>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥<br><br>🟨🟨🟨🟨🟨🟨🟨🟨🟨<br><br>🟩🟩🟩🟩🟩🟩🟩<br><br>🟦🟦🟦🟦🟦<br><br>🟪🟪🟪<br><br>⬛</p>"
+
+
+}
+
+
+
+// var b1 = document.getElementById("black");
+// var arr =[getElementById("black")];
+// push
+//  function move(){
+//      arr.pop();
+// }
